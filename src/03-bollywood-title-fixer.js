86e4732394380ad13e4571a22a5a7a90b7e8e62a
Kartik Kumar <kartikjhaba@gmail.com>
2026-02-14 00:14:55 +0530

    03-bollywood passed

--- src/03-bollywood-title-fixer.js
+++ src/03-bollywood-title-fixer.js
@@ -30,5 +30,30 @@
  *   // => "Dil ka Kya Kare"
  */
 export function fixBollywoodTitle(title) {
-  // Your code here
+  //validations
+  if(typeof title !== 'string') return '';
+
+  //removing trailing and leading spaces
+  title = title.trim();
+  
+  if(title === '') return '';
+
+  const requiredLowerCaseWords = ["ka", "ki", "ke", "se", "aur", "ya", "the", "of", "in", "a", "an"];
+
+  //removing middle spaces
+  const titleSplitArrayWithEmptyStrings = title.split(' ');
+  const words = titleSplitArrayWithEmptyStrings.filter(item => item != '');
+
+  //make first letter uppercase
+  const upperCaseWords = words.map(word => {
+    word = word.toLowerCase();
+    if(requiredLowerCaseWords.includes(word)) return word;
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  })
+
+  //making first word first letter uppercase
+  upperCaseWords[0] = upperCaseWords[0].charAt(0).toUpperCase() + upperCaseWords[0].slice(1);
+
+  //making string back from array
+  return upperCaseWords.join(' ');
 }
