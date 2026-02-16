3999838305bb44a4e6ef1f43e67c6f6e5ce3dfb8
Kartik Kumar <kartikjhaba@gmail.com>
2026-02-16 22:29:18 +0530

    10-upi passed

--- src/10-upi-transaction-log.js
+++ src/10-upi-transaction-log.js
@@ -47,5 +47,78 @@
  *   //      frequentContact: "Swiggy", allAbove100: false, hasLargeTransaction: true }
  */
 export function analyzeUPITransactions(transactions) {
-  // Your code here
+  //validations
+  if (!Array.isArray(transactions) || transactions.length === 0) return null;
+
+  //filtering invalid transactions
+  transactions = transactions.filter(transaction => (
+    transaction.amount > 0 && (transaction.type === 'credit' || transaction.type === 'debit')
+  ));
+
+  //checking transactions after filtering
+  if (transactions.length === 0) return null;
+
+  //calculations on valid transactions
+  const totalCredit = transactions
+    .filter(transaction => transaction.type === 'credit')
+    .reduce((acc, transaction) => acc + transaction.amount, 0);
+
+  const totalDebit = transactions
+    .filter(transaction => transaction.type === 'debit')
+    .reduce((acc, transaction) => acc + transaction.amount, 0);
+
+  const netBalance = totalCredit - totalDebit;
+
+  const transactionCount = transactions.length;
+
+  const avgTransaction = Math.round((totalCredit + totalDebit) / transactionCount);
+
+  //finding out highestTransaction (Descending sort)
+  transactions = transactions.sort((t1, t2) => t2.amount - t1.amount);
+  const highestTransaction = transactions[0];
+
+  //category breakdown
+  const categoryBreakdown = transactions.reduce((acc, transaction) => {
+    if (acc.hasOwnProperty(transaction.category)) {
+      acc[transaction.category] += transaction.amount;
+    } else {
+      acc[transaction.category] = transaction.amount;
+    }
+    return acc;
+  }, {})
+
+  //frequent contact
+  //reduce se grouped total nikal lunga
+  //fir usko based on count sort karunga descending me
+  //fir first element ko most frequent bana dunga
+  let groupedToCount = transactions.reduce((toCountObj, transaction) => {
+    if (toCountObj.hasOwnProperty(transaction.to)) {
+      toCountObj[transaction.to] += 1;
+    } else {
+      toCountObj[transaction.to] = 1;
+    }
+    return toCountObj
+  }, {})
+  groupedToCount = Object.entries(groupedToCount).sort((a, b) => b[1] - a[1]);
+  const frequentContact = groupedToCount[0][0];
+
+
+  //check if every transaction amount > 100
+  const allAbove100 = transactions.every(transaction => transaction.amount > 100)
+
+  //check if any transaction amount >= 5000
+  const hasLargeTransaction = transactions.some(transaction => transaction.amount >= 1000)
+
+  return { 
+    totalCredit,
+    totalDebit, 
+    netBalance, 
+    transactionCount, 
+    avgTransaction, 
+    highestTransaction, 
+    categoryBreakdown, 
+    frequentContact, 
+    allAbove100,
+    hasLargeTransaction
+  }
 }
